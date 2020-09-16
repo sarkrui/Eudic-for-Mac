956d784ecc26c49c1ddce3cd0ba7a973e7565646
sarkrui <sark.rui@gmail.com>
2020-09-16 19:31:22 +0200

    修正脚本

--- make.sh
+++ make.sh
@@ -5,4 +5,6 @@ shc -e 31/12/9999 -r -f Eudic.sh -o Release/Eudic && rm **.sh.x.c
 shc -e 31/12/9999 -r -f Eudic_cn.sh -o Release/Eudic_cn && rm **.sh.x.c 
 shc -e 31/12/9999 -r -f Activation.sh -o Release/Activation && rm **.sh.x.c 
 
+zip -r Release.zip Release
+
 echo "Compile finished."
